8acc9d94a4439747489729223f3a5c4a890183a2
Ankur Oberoi <aoberoi@gmail.com>
2016-01-18 16:16:04 -0800

    clean xcodeproj and xcworkspace. updated opentok dependency

---
+++ OpenTokRTC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenTokRTC
+//
+//  Created by Ankur Oberoi on 1/18/16.
+//  Copyright © 2016 Ankur Oberoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
